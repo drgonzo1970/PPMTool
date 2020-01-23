0c68daeed2ae23354f475a2bb0a0888dec6abd3c
mfranz <mfranz@ai-ag.de>
2020-01-23 13:20:32 +0100

    import bootstrap

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import ProjectItem from "./project/ProjectItem";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to the Dashboard</h1>
+        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
         <ProjectItem />
       </div>
     );
